5f075f716c246c0845d990ae7821d5b401d1b257
gr1ll0o <thiagotobias.grillo@gmail.com>
2025-12-01 20:12:08 -0300

    Web Errors Handler working

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
+let mainWindow;
+
 function createWindow() {
-  const win = new BrowserWindow({
+  mainWindow= new BrowserWindow({
     width: 1280,
     height: 720,
     autoHideMenuBar: true,
     useContentSize: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: false,
+      contextIsolation: true,
+      sandbox: false,
       webviewTag: true
     },
   });
 
-  win.loadFile("index.html");
-  //console.log("VENTANA CREADA")
+  mainWindow.loadFile("index.html");
+  //mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
   createWindow();
-  win.webContents.session.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36");
+  mainWindow.webContents.session.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36");
 });
--- main.js
+++ main.js
@@ -1,14 +1,76 @@
-let webView = document.getElementById('vista');
+// WEB VIEW
+const webView = document.getElementById('vista');
+const container = document.getElementById('container');
 
-let webInput = document.getElementById('webinput');
+// basic elements
+const webInput = document.getElementById('webinput');
 
-webinput.addEventListener('keydown', (event) => {
-    let query = webInput.value
+// error pages
+const nameNotResolved = document.getElementById('name-not-resolved');
+const internetDisconnected = document.getElementById('internet-disconnected');
+const connectionTimedOut = document.getElementById('connection-timed-out');
+
+/// FUNCTIONS
+function hideAllErrors() {
+    const errors = document.querySelectorAll('.error-page');
+    errors.forEach(err => {
+        err.style.display = 'none';
+    });
+}
+
+function isIP(str) {
+    return /^(\d{1,3}\.){3}\d{1,3}$/.test(str) && 
+           str.split('.').every(n => n >= 0 && n <= 255);
+}
+
+/// PROGRAM
+
+webInput.addEventListener('keydown', (event) => {
+    let query = webInput.value;
     if (event.key == 'Enter') {
-        if (!query.includes('https://')) {
-            webView.src = 'https://www.google.com/search?q=' + query
-        }else{
-            try { webView.src = query } catch (error) { console.log("ERROR: " + error) }
+        if (isIP(query)) {
+            if (!query.startsWith("http://") && !query.startsWith("https://")) {
+                query = "http://" + query;
+            }
+            webView.src = query;
+            return;
         }
+        
+        if (!query.includes('https://') && !query.includes('http://')) {
+            webView.src = 'https://www.google.com/search?q=' + query;
+        }else{
+            webView.src = query 
+        }
+
+        const raw = webView.src;
+        const url = new URL(raw);
+        webInput.value = url.protocol + "//" + url.hostname;
+
+        hideAllErrors();
+        container.style.display = 'block';
     }
 });
+webInput.addEventListener('click', () => {
+    webInput.value = webView.src;
+});
+
+webView.addEventListener('did-fail-load', (event) => {
+    container.style.display = 'none';
+    hideAllErrors();
+    console.log("did-fail-load:", event.errorDescription);
+    switch (event.errorDescription) {
+        case "ERR_NAME_NOT_RESOLVED":
+            nameNotResolved.style.display = 'block';
+            break;
+        case "ERR_INTERNET_DISCONNECTED":
+            internetDisconnected.style.display = 'block';
+            break;
+        case "ERR_CONNECTION_TIMED_OUT":
+            connectionTimedOut.style.display = 'block';
+            break;
+        case "ERR_CONNECTION_REFUSED":
+            connectionTimedOut.style.display = 'block';
+            break;
+    }    
+});
+
