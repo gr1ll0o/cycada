78095a2b5b1a0fe79a4ef94d1f75710d3159ca90
gr1ll0o <thiagotobias.grillo@gmail.com>
2025-12-01 15:42:08 -0300

    Added electron.js start script

---
+++ index.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("index.html");
+  //console.log("VENTANA CREADA")
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
