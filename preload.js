3e075b32f1c0d4a84bb1758678fb4da0b5669e20
gr1ll0o <thiagotobias.grillo@gmail.com>
2025-12-02 19:00:37 -0300

    Preload for Exit button

--- preload.js
+++ preload.js
@@ -1,3 +1,9 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
 window.addEventListener("DOMContentLoaded", () => {
     console.log("DOM listo");
+});
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  closeApp: () => ipcRenderer.send("close-app"),
 });
